18a30a9c113887636e7c038f3980cf80c120bb26
Andrew McNeil <andrew.richard.mcneil@gmail.com>
2022-03-21 09:54:33 -0700

    Add designer and developer credit

--- src/Main.elm
+++ src/Main.elm
@@ -191,15 +191,15 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model =
-    viewPhone model
-
-
-viewPhone : Model -> Html Msg
-viewPhone model =
     layout
         [ width fill
         , height fill
-        , padding <| padSm model
+        , paddingEach
+            { left = padSm model
+            , right = padSm model
+            , top = padSm model
+            , bottom = padXl model
+            }
         , htmlAttribute <|
             Html.Attributes.style "overflow" "scroll"
         , onEnter Calculate
@@ -212,24 +212,20 @@ viewPhone model =
             ]
             [ calculatorPanel model
             , infoPanel model
+            , developerLinks model
             ]
 
 
 calculatorPanel : Model -> Element Msg
 calculatorPanel model =
-    column
-        [ width fill
-        , padding <| padMd model
-        , Background.color grey
-        , Border.rounded 10
-        , Border.shadow panelShadow
-        ]
+    panel
         [ calculatorTitle model
         , numberInputs model
         , unitRadio model
         , calculateButton model
         , result model
         ]
+        model
 
 
 calculatorTitle : Model -> Element Msg
@@ -243,89 +239,6 @@ calculatorTitle model =
             ]
           <|
             text "1 RM Calculator"
-        ]
-
-
-infoPanel : Model -> Element Msg
-infoPanel model =
-    column
-        [ width fill
-        , padding <| padMd model
-        , Background.color grey
-        , Border.rounded 10
-        , Border.shadow panelShadow
-        ]
-        [ infoTitle model
-        , infoContent model
-        ]
-
-
-panelShadow :
-    { offset : ( number, number )
-    , size : number
-    , blur : number
-    , color : Color
-    }
-panelShadow =
-    { offset = ( 0, 4 )
-    , size = 0
-    , blur = 6
-    , color = blackTranslucent
-    }
-
-
-infoTitle : Model -> Element Msg
-infoTitle model =
-    row [ width fill ]
-        [ el
-            [ centerX
-            , Font.size <| fontLg model
-            , Font.letterSpacing 0.3
-            , Font.family fontPrimary
-            ]
-          <|
-            text "About 1 RM Calculator"
-        ]
-
-
-infoContent : Model -> Element Msg
-infoContent model =
-    let
-        paragraphWithStyle : String -> Element Msg
-        paragraphWithStyle content =
-            paragraph
-                [ spacing <| padXxs model
-                , Font.size <| fontMd model
-                , Font.family fontSecondary
-                , Font.alignLeft
-                ]
-                [ text content ]
-
-        moreInfoLink : Element Msg
-        moreInfoLink =
-            newTabLink
-                [ Font.size <| fontMd model
-                , Font.color blue
-                , Font.family fontSecondary
-                ]
-                { url = "https://en.wikipedia.org/wiki/One-repetition_maximum"
-                , label = text "More info about 1RM"
-                }
-    in
-    column
-        [ width fill
-        , paddingEach { edges | top = padMd model }
-        , spacing <| padMd model
-        ]
-        [ paragraphWithStyle
-            "One repetition maximum (one rep max or 1RM) in weight training is the maximum amount of weight that a person can possibly lift for one repetition. It may also be considered as the maximum amount of force that can be generated in one maximal contraction.  One repetition maximum can be used for determining an individuals maximum strength and is the method for determining the winner in events such as powerlifting and weightlifting competitions. One repetition maximum can also be used as an upper limit, in order to determine the desired load for an exercise (as a percentage of the 1RM)."
-        , paragraphWithStyle
-            "The 1RM can either be calculated directly using maximal testing or indirectly using submaximal estimation. The submaximal estimation method is preferred as it is safer, quicker, and less unnerving for inexperienced exercisers, however, it may underestimate the actual 1RM. One rep maximum calculators are used to predict a one rep maximum lift. The degree of accuracy can vary largely depending on the weight training experience and muscular composition of the athlete. Also, most one rep maximum calculators are designed for seasoned strength trainers, and those with little experience may find their actual one rep maximum is much lower because their nervous system cannot handle the stress of a high weight. This test should be performed with a spotter for reasons of safety."
-        , paragraphWithStyle
-            "Weight training protocols often use 1RM when programming to ensure the exerciser reaches resistance overload, especially when the exercise objective is muscular strength, endurance or hypertrophy. By understanding the maximal potential of the muscle, it is possible to reach resistance overload by increasing the number of repetitions for an exercise."
-        , paragraphWithStyle
-            "Determining the 1 rep max can be done directly through trial and error and simply requires the exerciser to complete one full repetition with the maximum weight. There are several common formulas used to estimate 1RM using the submaximal method, the Epley and the Brzycki being the most common. This app uses the Epley method."
-        , moreInfoLink
         ]
 
 
@@ -549,24 +462,6 @@ calculateButton model =
         ]
 
 
-onEnter : Msg -> Attribute Msg
-onEnter msg =
-    let
-        enterPressed =
-            Decode.field "key" Decode.string
-                |> Decode.andThen
-                    (\key ->
-                        if key == "Enter" then
-                            Decode.succeed msg
-
-                        else
-                            Decode.fail "Not the enter key"
-                    )
-    in
-    htmlAttribute <|
-        Html.Events.on "keyup" enterPressed
-
-
 result : Model -> Element Msg
 result model =
     let
@@ -597,10 +492,9 @@ result model =
                 , Font.family fontPrimary
                 ]
             <|
-                (text <|
+                text <|
                     String.fromInt model.oneRepMax
                         ++ unit
-                )
 
         label : Element Msg
         label =
@@ -650,6 +544,157 @@ result model =
                 ]
                 [ resetButton ]
             ]
+
+
+infoPanel : Model -> Element Msg
+infoPanel model =
+    panel
+        [ infoTitle model
+        , infoContent model
+        ]
+        model
+
+
+infoTitle : Model -> Element Msg
+infoTitle model =
+    row [ width fill ]
+        [ el
+            [ centerX
+            , Font.size <| fontLg model
+            , Font.letterSpacing 0.3
+            , Font.family fontPrimary
+            ]
+          <|
+            text "About 1 RM Calculator"
+        ]
+
+
+infoContent : Model -> Element Msg
+infoContent model =
+    let
+        paragraphWithStyle : String -> Element Msg
+        paragraphWithStyle content =
+            paragraph
+                [ spacing <| padXxs model
+                , Font.size <| fontMd model
+                , Font.family fontSecondary
+                , Font.alignLeft
+                ]
+                [ text content ]
+
+        moreInfoLink : Element Msg
+        moreInfoLink =
+            newTabLink
+                [ Font.size <| fontLg model
+                , Font.color blue
+                , Font.family fontSecondary
+                ]
+                { url = "https://en.wikipedia.org/wiki/One-repetition_maximum"
+                , label = text "More info about 1RM"
+                }
+    in
+    column
+        [ width fill
+        , paddingEach { edges | top = padMd model }
+        , spacing <| padMd model
+        ]
+        [ paragraphWithStyle
+            "A one repetition maximum (one rep max or 1RM) in weight training is the maximum amount of weight that a person can possibly lift for one repetition. It can be used for determining an individuals maximum strength and is the method for determining the winner in events such as powerlifting and weightlifting competitions. A one repetition maximum can also be used as an upper limit, in order to determine the desired load for an exercise (as a percentage of the 1RM)."
+        , paragraphWithStyle
+            "The 1RM can either be calculated directly using maximal testing or indirectly using submaximal estimation. The submaximal estimation method is preferred as it is safer, quicker, and less unnerving for inexperienced exercisers, however, it may underestimate the actual 1RM. One rep maximum calculators are used to predict a one rep maximum lift. The degree of accuracy can vary largely depending on the weight training experience and muscular composition of the athlete. Also, most one rep maximum calculators are designed for seasoned strength trainers, and those with little experience may find their actual one rep maximum is much lower because their nervous system cannot handle the stress of a high weight. This test should be performed with a spotter for reasons of safety."
+        , paragraphWithStyle
+            "There are several common formulas used to estimate 1RM using the submaximal method, the Epley and the Brzycki being the most common. This app uses the Epley method."
+        , moreInfoLink
+        ]
+
+
+developerLinks : Model -> Element Msg
+developerLinks model =
+    let
+        linkStyle : List (Attribute Msg)
+        linkStyle =
+            [ Font.center
+            , Font.color blue
+            , Font.size <| fontMd model
+            , Font.family fontSecondary
+            , Font.bold
+            ]
+
+        textStyle : List (Attribute Msg)
+        textStyle =
+            [ Font.center
+            , Font.color black
+            , Font.size <| fontMd model
+            , Font.family fontSecondary
+            ]
+
+        designerLink : Element Msg
+        designerLink =
+            newTabLink
+                linkStyle
+                { url = "https://www.mariayevickery.com"
+                , label = text "Mariaye Vickery"
+                }
+
+        developerLink : Element Msg
+        developerLink =
+            newTabLink
+                linkStyle
+                { url = "https://www.github.com/armcn"
+                , label = text "Andrew McNeil"
+                }
+    in
+    column
+        [ width fill
+        , spacing <| padXs model
+        ]
+        [ paragraph textStyle
+            [ text "Product designed by "
+            , designerLink
+            ]
+        , paragraph textStyle
+            [ text "Built with elm-lang and elm-ui by "
+            , developerLink
+            ]
+        ]
+
+
+panel : List (Element Msg) -> Model -> Element Msg
+panel children model =
+    let
+        panelShadow =
+            { offset = ( 0, 4 )
+            , size = 0
+            , blur = 6
+            , color = blackTranslucent
+            }
+    in
+    column
+        [ width fill
+        , padding <| padMd model
+        , Background.color grey
+        , Border.rounded 10
+        , Border.shadow panelShadow
+        ]
+        children
+
+
+onEnter : Msg -> Attribute Msg
+onEnter msg =
+    let
+        enterPressed =
+            Decode.field "key" Decode.string
+                |> Decode.andThen
+                    (\key ->
+                        if key == "Enter" then
+                            Decode.succeed msg
+
+                        else
+                            Decode.fail "Not the enter key"
+                    )
+    in
+    htmlAttribute <|
+        Html.Events.on "keyup" enterPressed
 
 
 toSvgColor : Color -> String
